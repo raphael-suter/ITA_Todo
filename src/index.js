df02fc0dd97ee259c2d52f2f631a6cc69f9ae0ea
Raphael Suter <raphael.werner.suter@gmail.com>
2021-01-06 14:10:00 +0100

    Aufgabe 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Card, Container, Form, ListGroup } from "react-bootstrap";
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [value, setValue] = useState("");
+
+  const addTodo = () => {
+    setTodos([value, ...todos]);
+    setValue("");
+  };
+
+  const deleteTodo = (index) => {
+    const list = todos.slice();
+    list.splice(index, 1);
+
+    setTodos(list);
+  };
+
+  return (
+    <Container className="my-5">
+      <Card>
+        <Card.Header className="bg-info text-light font-weight-bold">
+          Todos
+        </Card.Header>
+        <Card.Body>
+          <Form className="d-flex">
+            <Form.Group className="flex-fill mb-0 mr-3">
+              <Form.Control
+                type="text"
+                value={value}
+                onChange={(evt) => setValue(evt.target.value)}
+              />
+            </Form.Group>
+            <Button variant="info" disabled={value === ""} onClick={addTodo}>
+              +
+            </Button>
+          </Form>
+          {todos.length > 0 && (
+            <ListGroup className="mt-3">
+              {todos.map((todo, index) => (
+                <ListGroup.Item
+                  className="d-flex justify-content-between align-items-center"
+                  key={index}
+                >
+                  {todo}
+                  <Button variant="danger" onClick={() => deleteTodo(index)}>
+                    Delete
+                  </Button>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          )}
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
